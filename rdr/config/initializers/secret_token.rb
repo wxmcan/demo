ebba367f127bea23b3abf0aa990df02c060cd7c0
wxmcan <wxmcan@163.com>
2011-12-29 07:54:08 +0800

    first commit

---
+++ rdr/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rdr::Application.config.secret_token = '7f49a28f1ea8a3555671b93cb3b436ea84d2afdd69f84f2487fc2c84ad7e4f5f7933bf9223ad35a4c67e2ec8ecd09c9049bfaef5f73d994d323272fbb8c8a7e2'
