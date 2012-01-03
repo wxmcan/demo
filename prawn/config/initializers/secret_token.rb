68676482be323b30390e948ce5ea1807efdc3d1b
wxmcan <wxmcan@163.com>
2012-01-03 13:14:53 +0800

    init prawn project

---
+++ prawn/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Prawn::Application.config.secret_token = '310e3613d25c720586e745f03b3785f54006ab6621b89d0883946867646c70f2546dd07110589b7c9891f22da91d880108d3d20ffaaf32e54a783ed53d26ac54'
