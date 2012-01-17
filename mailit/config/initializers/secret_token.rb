955ca3c3a779491fc7b376d23fe1cf7cad6ec6c2
wxmcan <wxmcan@163.com>
2012-01-17 09:13:41 +0800

    first commit

---
+++ mailit/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mailit::Application.config.secret_token = '16ea789129bf3cee61c65471a71310b8367e96f63928bbe6189ab314a3918355f73dd0e6e453f8091b2252fa14fa49a7dd5fc1cf4087f178bf95dc4780023c0d'
