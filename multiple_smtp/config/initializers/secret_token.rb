243fd0736327bcdd2f05164ea548ff6595e8652a
wxmcan <wxmcan@163.com>
2012-02-08 17:38:40 +0800

    adding multiple_smtp

---
+++ multiple_smtp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MultipleSmtp::Application.config.secret_token = '2cb0e8dac7559bbe7e64a5b80c22b72c2ada3d077a00412980d2291887f9b3e73cfd87a994eb5db284ac4a671680e2e0d9ff52a52d808e040e7d9df5f7f06bc7'
