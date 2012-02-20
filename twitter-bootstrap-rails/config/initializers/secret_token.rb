89945af74911e3bc7febe8995031b647f517b94f
wxmcan <wxmcan@163.com>
2012-02-20 11:57:27 +0800

    adding twitter-bootstrap-rails project

---
+++ twitter-bootstrap-rails/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitterBootstrapRails::Application.config.secret_token = '8fc61fe18df08d02009fe79fe00ff9548a83f8c6b5866fb53a086db93e97061862ceca474752b6a1867fc9de16cdd40edf66106ee45dd35ea81e69db7a6fad9f'
