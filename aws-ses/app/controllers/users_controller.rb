713c7ef1ef178c45452d012b98a5de09cbf326f8
wxmcan <wxmcan@163.com>
2012-03-14 17:26:23 +0800

    adding send mail code

--- aws-ses/app/controllers/users_controller.rb
+++ aws-ses/app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
+      UserMailer.welcome_email(@user).deliver
       redirect_to @user, :notice => "Successfully created user."
     else
       render :action => 'new'
--- aws-ses/app/models/user.rb
+++ aws-ses/app/models/user.rb
@@ -1,12 +1,8 @@
 class User < ActiveRecord::Base
   attr_accessible :login, :email
 
-  def self.test
-    ses = AWS::SES::Base.new
-    p ses.addresses.list.result
-#    ses.send_email :to        => ['bob.wang@beltal.com'],
-      #:source    => '"Steve Smith" <steve@example.com>',
-      #:subject   => 'Subject Line'
-#    :text_body => 'Internal text body'
+  def self.t
+    user = User.first
+    UserMailer.welcome_email(user).deliver
   end
 end
