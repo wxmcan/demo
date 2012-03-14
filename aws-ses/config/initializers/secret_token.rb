5c72ee697e6caed2063d6922d52602d4fc529e1c
wxmcan <wxmcan@163.com>
2012-03-14 15:57:31 +0800

    init aws-ses project

---
+++ aws-ses/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AwsSes::Application.config.secret_token = 'cd6c682f4c617428761154bf33c85a03cfe14ec70589941711652188e3f1d1946f4fc6a8a83a9890142b3b7a15f37d0db3268c95ed49f425fbf927ec849b6bb1'
