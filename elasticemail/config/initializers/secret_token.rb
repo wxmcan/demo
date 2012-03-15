d437fec14f7c7dc2df31d9f48f2439d032513d9c
wxmcan <wxmcan@163.com>
2012-03-15 15:13:14 +0800

    init elasticemail project

---
+++ elasticemail/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Elasticemail::Application.config.secret_token = '2a0305e72a10d71d3b55f5694afcf293880bbd0fb2231f6757c29a78b0dd5034158ad64a866fae424cfed9908c92cdd69fd4840a5183b8df9de350ba1b38b4a9'
