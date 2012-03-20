ba300007a197c62a7151c09acb1af4a7fe876998
wxmcan <wxmcan@163.com>
2012-03-20 22:37:07 +0800

    init refinery-existing project

---
+++ refinery-existing/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RefineryExisting::Application.config.secret_token = '810a9ec1aa510469ad91c1d77e938d277cd61d7ecb83794b287532b5688bd0f149141e32e58f03b7dc6d16a50b5337abdd4e260d26c920a23e47dd70a1016d41'
