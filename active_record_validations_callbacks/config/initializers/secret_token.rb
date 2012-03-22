da478a655c228148be92d50c23c7b087572110b5
wxmcan <wxmcan@163.com>
2012-03-22 16:26:07 +0800

    init active_record_validations_callbacks project

---
+++ active_record_validations_callbacks/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ActiveRecordValidationsCallbacks::Application.config.secret_token = 'c0dce2e55bf67bc9054b0e5fffc8bf47af4bac38b115c6a8b635b2547e666313a189662d1837973d170df004e5a74d2c03908325549e3a6002ca0dd9ae6f3ca1'
---
+++ active_record_validations_callbacks/config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ActiveRecordValidationsCallbacks::Application.config.session_store :cookie_store, key: '_active_record_validations_callbacks_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ActiveRecordValidationsCallbacks::Application.config.session_store :active_record_store
