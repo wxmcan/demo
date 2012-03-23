0ef1c4133a1d6dd884a5f8f8083bb4576c9f0818
wxmcan <wxmcan@163.com>
2012-03-23 11:27:26 +0800

    init vanity project

---
+++ vanity/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vanity::Application.config.secret_token = '5805c69785ebd07c2389dd6ce8bce2b870385d423b1274a725fbdade19a3218c676fb32f011eeb0ed56a2df310aef87546aaba07c199c9a1a91e87cdd1acf568'
