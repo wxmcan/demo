7587a92be6688846244537b93c7d7ef9be9f9dea
wxmcan <wxmcan@163.com>
2012-03-23 10:08:06 +0800

    init split project

---
+++ split/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Split::Application.config.secret_token = '3ba8b56aad276c3489551314c14714e5325e25c1092dc0e0e8b0d4cefd29fe8ff58a3e5283012385d10e9abbd8663877dc7008faa0782afe86e8f5019e8ba2fc'
