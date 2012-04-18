fc0a7105f868f4e80bfea6e9a1f38e859e50ae04
Bob Wang <wxmcan@163.com>
2012-04-18 11:20:58 +0800

    cumstom devise views

--- refinerycms-devise/db/seeds.rb
+++ refinerycms-devise/db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+me = User.create(:email=>"bob.wang@beltal.com", :password=>"1234qaz")
