0bb3d0a84978a69bb90deb9826b8f529be663fa3
Bob Wang <wxmcan@163.com>
2012-04-22 07:13:16 +0800

    init devise-refinery

---
+++ devise-refinery/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeviseRefinery::Application.config.secret_token = '3de95ad2d85a908abfcae30ab05fe3fddebad0d57ed6ccfc2650137a209c063da3c9998b9452ae01b405763dd605bd8d9390e2a6743b50e50979ac9666ec9605'
