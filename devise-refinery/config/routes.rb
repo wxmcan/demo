f6af33992e64214b84a0e4cd571b2f527f87350f
Bob Wang <wxmcan@163.com>
2012-04-24 12:09:01 +0800

    integration refinery and devise

--- devise-refinery/config/routes.rb
+++ devise-refinery/config/routes.rb
@@ -1,5 +1,8 @@
 DeviseRefinery::Application.routes.draw do
+  resources :products
+
   devise_for :users
+  root :to => 'products#index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
