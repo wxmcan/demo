d5e36139e92113b9b54c92661395e11a28c4c2ba
wxmcan <wxmcan@163.com>
2012-07-13 11:08:24 +0800

    init taobaoke project

---
+++ taobaoke/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Taobaoke::Application.config.secret_token = '6611b423006c07e710fe2feb3cffef3c3c93804f6a7e03a9a417f29ecf01ebaa0c611dd9eed94c5184b6168838af790624f214c0458ed52aa1655d61a23f6b8d'
