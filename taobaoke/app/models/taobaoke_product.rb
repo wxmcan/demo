2d40d0fa8623fb6fd6d173cad8f6a9b4ce00e9f1
wxmcan <wxmcan@163.com>
2012-07-16 18:18:48 +0800

    fix taobaoke invoke bug

--- taobaoke/app/models/taobaoke_product.rb
+++ taobaoke/app/models/taobaoke_product.rb
@@ -1,14 +1,23 @@
 class TaobaokeProduct < ActiveRecord::Base
   attr_accessible :num_iid, :title, :nick, :price, :pic_url, :click_url, :commission, :commission_rate, :commission_num, :commission_volume, :coupon_price, :coupon_rate, :coupon_start_time, :coupon_end_time, :volume, :shop_click_url, :seller_credit_score, :item_location, :shop_type, :taobaoke_cat_click_url, :keyword_click_url
+  FIELDS = "num_iid,title,nick,pic_url,price,click_url,commission,commission_rate,commission_num,commission_volume,shop_click_url,seller_credit_score,item_location,volume,coupon_price,coupon_rate,coupon_start_time,coupon_end_time,shop_type"
 
-  def self.i 
-    ::Taobao::Client.new.invoke('taobao.taobaoke.items.get', { :cid => 16, :nick => 'quickerbuy' })["taobaoke_items_get_response"]
+  def coupon
+    options = {:fields => FIELDS}.merge(get_options)
+    Taobao::Client.new.invoke('taobao.taobaoke.items.coupon.get', { :cid => 16, :nick => 'quickerbuy' }) #["taobaoke_items_get_response"]
   end
 
+  def get_options
+    { :cid => 16, :nick => 'quickerbuy' }
+  end
 
-  def self.t(cid = 16)
-    options = {:category_id => cid, :recommend_type => 1, :count => 20}
-    Taobao::Client.new.invoke("taobao.categoryrecommend.items.get" , options)['favorite_items']['favorite_item'] 
+  def convert
+    options = { :num_iids => '15430371400', :nick => 'quickerbuy', :fields => FIELDS }
+    p Taobao::Client.new.invoke("taobao.taobaoke.items.convert" , options)
+  end
+
+  def save_coupon_items
+
   end
 
 end
